af27d0f538977b2e259db1d058bf7a7a5584dd8e
Otto Mäkitalo <otto.makitalo@tuni.fi>
2023-11-14 14:17:50 +0200

    Cloned template for project and add.test.js now ready to be implemented

--- tests/add.test.js
+++ tests/add.test.js
@@ -1,30 +1,12 @@
+import { expect } from 'chai';
+
 // Import the add function from add.js
-import add from '../functions/add';
+import add from '../src/add.js';
 
 // Describe block for the add function
 describe('add function', () => {
   // Test case 1
-  test('adds two positive numbers', () => {
-    expect(add(6, 4)).toBe(10);
-  });
-
-  // Test case 2
-  test('adds a positive and a negative number', () => {
-    expect(add(6, -4)).toBe(2);
-  });
-
-  // Test case 3
-  test('adds two negative numbers', () => {
-    expect(add(-6, -4)).toBe(-10);
-  });
-
-  // Test case 4
-  test('adds zero and a number', () => {
-    expect(add(0, 5)).toBe(5);
-  });
-
-  // Test case 5
-  test('adds zero and zero', () => {
-    expect(add(0, 0)).toBe(0);
+  it('should pass the sample test', () => {
+    expect(add(1,1)).to.equal(2);
   });
 });
