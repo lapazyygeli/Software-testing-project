a261995a18c43a89f2e5be908a8043bde8118e10
Lassi Helen <lassi.helen@tuni.fi>
2023-11-14 15:39:01 +0200

    Ceil function tested

---
+++ tests/ceil.test.js
@@ -0,0 +1,54 @@
+import { expect } from 'chai';
+
+// Import the ceil function from ceil.js
+import ceil from '../src/ceil.js';
+
+describe('Ceil function', () => {
+  it('Should round up a positive decimal number to the nearest integer', () => {
+    expect(ceil(4.006)).to.equal(5);
+  });
+
+  it('Should round up a positive decimal number to a specified precision', () => {
+    expect(ceil(6.004, 2)).to.equal(6.01);
+  });
+
+  it('Should round up a positive integer to a specified negative precision', () => {
+    expect(ceil(6040, -2)).to.equal(6100);
+  });
+
+  it('Should round up a negative decimal number to the nearest integer', () => {
+    expect(ceil(-4.006)).to.equal(-4);
+  });
+
+  it('Should round up a negative decimal number to a specified precision', () => {
+    expect(ceil(-6.004, 2)).to.equal(-6);
+  });
+
+  it('Should round up a negative integer to a specified negative precision', () => {
+    expect(ceil(-6040, -2)).to.equal(-6000);
+  });
+
+  it('Should round up zero to the nearest integer', () => {
+    expect(ceil(0)).to.equal(0);
+  });
+
+  it('Should round up zero to a specified precision', () => {
+    expect(ceil(0, 2)).to.equal(0);
+  });
+
+  it('Should round up zero to a specified negative precision', () => {
+    expect(ceil(0, -2)).to.equal(0);
+  });
+
+  it('Should handle Infinity by returning Infinity', () => {
+    expect(ceil(Infinity)).to.equal(Infinity);
+  });
+
+  it('Should handle negative Infinity by returning negative Infinity', () => {
+    expect(ceil(-Infinity)).to.equal(-Infinity);
+  });
+
+  it('Should handle NaN by returning NaN', () => {
+    expect(isNaN(ceil(NaN))).to.be.true;
+  });
+});
