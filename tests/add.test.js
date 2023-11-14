cbd81b1c88ce32d2ad2604027ae5812f80177dac
Otto Mäkitalo <otto.makitalo@tuni.fi>
2023-11-14 15:11:18 +0200

    add.test is ready

--- tests/add.test.js
+++ tests/add.test.js
@@ -3,10 +3,66 @@ import { expect } from 'chai';
 // Import the add function from add.js
 import add from '../src/add.js';
 
-// Describe block for the add function
-describe('add function', () => {
-  // Test case 1
-  it('should pass the sample test', () => {
+describe('Add function', () => {
+  it('Adds two numbers together and returns the sum', () => {
     expect(add(1, 1)).to.equal(2);
+  });
+
+  it('Negative numbers can be used', () => {
+    expect(add(-1, -1)).to.equal(-2);
+  });
+
+  it('Must check that the parameters are really numbers', () => {
+    expect(add('a', '🤣')).to.not.equal('a🤣');
+  });
+
+  it('Add is given numbers as string', () => {
+    expect(add('1', '1')).to.equal(2);
+  });
+
+  it('Should return the second parameter when null is given as the other parameter', () => {
+    expect(add(null, 1)).to.equal(1);
+  });
+
+  it('Should return the second parameter when Boolean is given as the other parameter', () => {
+    expect(add(true, 1)).to.equal(1);
+  });
+
+  it('Should return Infinity when Infinity is added', () => {
+    expect(add(Infinity, 1)).to.equal(Infinity);
+    expect(add(Infinity, Infinity)).to.equal(Infinity);
+  });
+
+  it('Infinity should not be added to minus infinity', () => {
+    expect(add(Infinity, -Infinity)).to.be.NaN;
+  });
+
+  it('Adding binary and hexadecimal numbers', () => {
+    expect(add(0x10, 0b10)).to.equal(18);
+  });
+
+  it('Sum with numbers in scientific notation', () => {
+    expect(add(1e5, 2e5)).to.equal(3e5);
+  });
+
+  it('Adding infinite decimal numbers', () => {
+    const PI = Math.PI;
+    const E = Math.E;
+
+    expect(add(PI, E)).to.be.closeTo(PI + E, 0.0000001);
+  });
+
+  it('Add is given two different kinds of numbers', () => {
+    expect(add(1.5, 2)).to.equal(3.5);
+  }); 
+
+  it('Should add the biggest numbers without overflowing', () => {
+    const result = add(Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER);
+    expect(result).to.equal(2 * Number.MAX_SAFE_INTEGER);
+  });
+
+  it('Should add the smallest numbers without overflowing', () => {
+    const result = add(Number.MIN_SAFE_INTEGER, Number.MIN_SAFE_INTEGER);
+    expect(result).to.equal(2 * Number.MIN_SAFE_INTEGER);
   });
 });
