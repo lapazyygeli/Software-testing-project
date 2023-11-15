cea9f1e7081ae24a5f09406a6029cdf8f826108f
Otto Mäkitalo <otto.makitalo@tuni.fi>
2023-11-15 09:13:56 +0200

    toString.test complete

--- tests/toString.test.js
+++ tests/toString.test.js
@@ -2,8 +2,28 @@ import { expect } from 'chai';
 
 import toString from '../src/toString.js';
 
-describe('toNumber function', () => {
-    it('', () => {
+describe('toString function', () => {
+    it('Should convert number into string', () => {
+        expect(toString(-0)).to.equal('-0');
+    })
 
+    it('Should convert array into string', () => {
+        expect(toString([1, 2, 3])).to.equal('1,2,3');
+    })
+
+    it('Should return arrays inside arrays to string', () => {
+        expect(toString([1, [2, [3]]])).to.equal('1,2,3');
+    })
+
+    it('Should convert null into empty string', () => {
+        expect(toString(null)).to.equal('');
+    })
+
+    it('Should return same string when string is given', () => {
+        expect(toString('hello')).to.equal('hello');
+    })
+
+    it('Should convert Infinity to string', () => {
+        expect(toString(Infinity)).to.equal('Infinity');
     })
 });
